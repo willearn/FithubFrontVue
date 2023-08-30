787fbebd502d66c688e8c9b53741cfb90457895d
william <cc2260112@gmail.com>
2023-08-30 22:47:21 +0800

    新增 1.租借場地日曆

--- src/views/rent.vue
+++ src/views/rent.vue
@@ -1,7 +1,3 @@
-<script setup>
-
-</script>
-
 <template>
     <!-- 輪播圖 -->
     <div class="masthead">
@@ -40,32 +36,33 @@
 
 
     <!-- rent-->
-    <section class="page-section mybg-light" id="rent">
+    <section class="page-section" id="rent">
         <div class="container text-center">
-            <h1>場地租借</h1>
-            <hr class="divider" />
-            <div class="col-lg-12 col-md-12">
-                
-                    
-                    場地:
-                    <select class="form-select-lg me-sm-5">
-                        <option>攀岩教室</option>
-                        <option>有氧大教室</option>
-                        <option>有氧小教室</option>
-                    </select>
-                    日期:
-                 <input type="date" class="form-control-lg me-sm-5">
-                    時段:
-                    <select class="form-select-lg me-sm-5">
-                        <option>上午</option>
-                        <option>下午</option>
-                        <option>晚上</option>
-                    </select>
-                    <button type="submit" class="btn btn-primary">預約</button>
-                
+            <div class="col-lg-12 col-md-10">
+                <h1>場地租借</h1>
+                <hr class="divider" />
+                <label class="fs-3">場地:</label>
+                <select class="form-select-lg me-sm-5">
+                    <option>攀岩教室</option>
+                    <option>有氧大教室</option>
+                    <option>有氧小教室</option>
+                </select>
+                <label class="fs-3">日期:</label>
+                <input type="date" class="form-control-lg me-sm-5" id="dateInput">
+                <label class="fs-3">時段:</label>
+                <select class="form-select-lg me-sm-5">
+                    <option>上午</option>
+                    <option>下午</option>
+                    <option>晚上</option>
+                </select>
+                <button type="submit" class="btn btn-primary btn-lg">預約</button>
+
+                <div id="calendar" class="calendar text-center mt-5">
+                </div>
             </div>
         </div>
     </section>
+
 
     <!-- rent-->
     <section class="page-section" id="rent">
@@ -86,7 +83,6 @@
                         </tr>
                     </thead>
                     <tbody>
-
                         <tr>
                             <td class="col-lg-2 col-md-2">攀岩教室</td>
                             <td class="col-lg-2 col-md-2">15</td>
@@ -117,3 +113,76 @@
         </div>
     </section>
 </template>
+
+
+<script setup>
+
+import { Calendar } from '@fullcalendar/core'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import { reactive, ref, onMounted } from 'vue'
+
+
+
+onMounted(() => {
+    //掛載日曆物件
+    const calendarEl = document.getElementById('calendar');
+    const calendar = new Calendar(calendarEl, {
+        plugins: [dayGridPlugin],
+        initialView: 'dayGridMonth',
+        timeZone: 'UTC',
+        locale: 'zh-tw',
+        height: 700,
+        headerToolbar: false,
+        events: [
+            {
+                title: '上午:已預定',
+                start: '2023-08-29',
+            },
+            {
+                title: '下午:已預定',
+                start: '2023-08-29',
+            },
+            {
+                title: '晚上:已預定',
+                start: '2023-08-29',
+                url: 'http://google.com/',
+            },
+        ],
+        eventOrder: function (a, b) {
+            const order = { '早上:已預定': 1, '中午:已預定': 2, '晚上:已預定': 3 };
+            return order[a.title] - order[b.title];
+        },
+    });
+    calendar.render();
+
+
+
+    //日期只顯示下個月
+    const today = new Date();
+    const currentMonth = today.getMonth();
+    const currentYear = today.getFullYear();
+
+    let nextMonth = currentMonth + 1;
+    let nextYear = currentYear;
+
+    const daysInMonth = new Date(nextYear, nextMonth + 1, 0).getDate();
+
+    let nextMonthFormatted = (nextMonth + 1).toString().padStart(2, '0'); // 格式化為雙位數
+    let lastDayOfNextMonthFormatted = daysInMonth.toString().padStart(2, '0'); // 格式化為雙位數
+
+    const dateInput = document.getElementById('dateInput');
+    dateInput.min = `${nextYear}-${nextMonthFormatted}-01`; // 下個月的第一天
+    dateInput.max = `${nextYear}-${nextMonthFormatted}-${lastDayOfNextMonthFormatted}`; // 下個月的最後一天
+
+
+});
+
+</script>
+
+<style scoped>
+.calendar {
+    background-color: #fefffa;
+}
+</style>
+
+
