fe8bc83a3d709468d14542071cb591705719813b
CR4-08\iSpan <love5216390@gmail.com>
2023-08-31 15:27:15 +0800

    購物車 結帳頁面 確認頁面

--- src/views/orderCheck.vue
+++ src/views/orderCheck.vue
@@ -33,14 +33,91 @@
         </div>
     </div> -->
 
-    <!-- 場地租借 -->
+    <!-- 場地租借 --><!-- 結帳頁面 -->
     <section class="page-section" id="about">
         <div class="container">
-            <div class="col-lg-4 col-md-4">
-                123
-            </div>
-            <div class="col-lg-8 col-md-8" style="background-color: #FFBF3C;">
-                123
+            <div style="position:relative; top: 20%; left: 350px;">
+                <div class="position-relative m-5">
+                    <div class="progress" style="height: 15px;width: 1050px;">
+                        <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="75"
+                            aria-valuemin="0" aria-valuemax="100">75%
+                        </div>
+                    </div>
+                </div>
+                <div class="d-grid gap-3" style="width: 50%; height: 60%; ">
+                    <div class="p-2 bg-light border">
+                        <ul class="nav">
+                            <li class="nav-item">
+                                <a class="nav-link active disabled" aria-current="page" href="#"><u>購物車</u></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><u>我的願望清單</u></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><u>我的折價券</u></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div style="width: 12rem;position: absolute;right: 38%;">
+                        <div class="card text-center">
+                            <div class="card-body">
+                                <h5 class="card-title">我的訂單</h5>
+                                <p class="card-text">總金額:</p>
+                                <a href="#" class="btn btn-primary">結帳</a>
+                            </div>
+                        </div>
+                        <div style="position:relative; top: 20%; left: 50px;">
+
+                            <router-link class="btn btn-secondary mt-5" to="/course">繼續購物</router-link>
+                        </div>
+                    </div>
+                    <div class="p-2 bg-light border">
+                        <table class="table align-middle">
+                            <thead class="table-light">
+                                <tr>
+                                    <th scope="col">#</th>                                    
+                                    <th scope="col">課程名稱</th>
+                                    <th scope="col">課程教練</th>
+                                    <th scope="col">課程時間</th>
+                                    <th scope="col">單價</th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                    <td>$2000</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                    <td>$2000</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td colspan="2">Larry the Bird</td>
+                                    <td>@twitter</td>
+                                    <td>$2000</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="p-2 bg-light border">
+                        <div class="form-floating">
+                            <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                                <option selected>綠界金流</option>                                
+                            </select>
+                            <label for="floatingSelect">Pay with :</label>
+                        </div>
+                    </div>
+
+                </div>
             </div>
             <router-link class="btn btn-secondary mt-5" to="/">跳轉金流 結帳成功回首頁</router-link>
         </div>
