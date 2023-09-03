663cd43ac3ff12975c4f0c1a552dcfb66ac42621
william <cc2260112@gmail.com>
2023-09-03 11:09:55 +0800

    新增 首頁串接google map

--- src/views/index.vue
+++ src/views/index.vue
@@ -174,81 +174,94 @@
   <section class="page-section" id="contact">
     <div class="container px-4 px-lg-5">
       <div class="row gx-4 gx-lg-5 justify-content-center">
-        <div class="col-lg-12 col-xl-12 text-center">
+        <div class="col-lg-6 col-xl-6">
+          <GoogleMap api-key="AIzaSyB3Hjtldwb3Ep_QOvoCs1UZAtkmWNDCJyA" style="width: 100%; height: 100%" :center="center"
+            :zoom="15">
+            <Marker :options="{ position: center }">
+              <InfoWindow>
+                <div id="contet" class="fs-5">
+                  <h4 class="text-center">Fithub健身房</h4>
+                  <p><i class="bi bi-telephone-inbound-fill"></i> 02-2468-0666</p>
+                  <p><i class="bi bi-clock-fill"></i>周一至周六 06:00~24:00周日 08:00~24:00</p>
+                  <p><i class="bi bi-geo-alt-fill"></i>基隆市信義區東明路177號3F</p>
+                </div>
+              </InfoWindow>
+            </Marker>
+          </GoogleMap>
+        </div>
+        <div class="col-lg-6 col-xl-6 text-center">
           <div class="text-center pt-1">
             <h2 class="mt-0">聯絡我們</h2>
             <hr class="divider" />
           </div>
-          <p class="text-black-75 mb-5">Send us a messages and we will get back to you as soon as possible!</p>
           <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
-            <div class="col-lg-6">
-              <!-- * * * * * * * * * * * * * * *-->
-              <!-- * * SB Forms Contact Form * *-->
-              <!-- * * * * * * * * * * * * * * *-->
-              <!-- This form is pre-integrated with SB Forms.-->
-              <!-- To make this form functional, sign up at-->
-              <!-- https://startbootstrap.com/solution/contact-forms-->
-              <!-- to get an API token!-->
-              <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                <!-- Name input-->
-                <div class="form-floating mb-3">
-                  <input class="form-control" id="name" type="text" placeholder="Enter your name..."
-                    data-sb-validations="required" />
-                  <label for="name">Full name</label>
-                  <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.
-                  </div>
+
+            <!-- * * * * * * * * * * * * * * *-->
+            <!-- * * SB Forms Contact Form * *-->
+            <!-- * * * * * * * * * * * * * * *-->
+            <!-- This form is pre-integrated with SB Forms.-->
+            <!-- To make this form functional, sign up at-->
+            <!-- https://startbootstrap.com/solution/contact-forms-->
+            <!-- to get an API token!-->
+            <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+              <!-- Name input-->
+              <div class="form-floating mb-3">
+                <input class="form-control" id="name" type="text" placeholder="Enter your name..."
+                  data-sb-validations="required" />
+                <label for="name">Full name</label>
+                <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.
                 </div>
-                <!-- Email address input-->
-                <div class="form-floating mb-3">
-                  <input class="form-control" id="email" type="email" placeholder="name@example.com"
-                    data-sb-validations="required,email" />
-                  <label for="email">Email address</label>
-                  <div class="invalid-feedback" data-sb-feedback="email:required">An email is
-                    required.</div>
-                  <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.
-                  </div>
+              </div>
+              <!-- Email address input-->
+              <div class="form-floating mb-3">
+                <input class="form-control" id="email" type="email" placeholder="name@example.com"
+                  data-sb-validations="required,email" />
+                <label for="email">Email address</label>
+                <div class="invalid-feedback" data-sb-feedback="email:required">An email is
+                  required.</div>
+                <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.
                 </div>
-                <!-- Phone number input-->
-                <div class="form-floating mb-3">
-                  <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890"
-                    data-sb-validations="required" />
-                  <label for="phone">Phone number</label>
-                  <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is
-                    required.</div>
+              </div>
+              <!-- Phone number input-->
+              <div class="form-floating mb-3">
+                <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890"
+                  data-sb-validations="required" />
+                <label for="phone">Phone number</label>
+                <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is
+                  required.</div>
+              </div>
+              <!-- Message input-->
+              <div class="form-floating mb-3">
+                <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..."
+                  style="height: 10rem" data-sb-validations="required"></textarea>
+                <label for="message">Message</label>
+                <div class="invalid-feedback" data-sb-feedback="message:required">A message is
+                  required.</div>
+              </div>
+              <!-- Submit success message-->
+              <!---->
+              <!-- This is what your users will see when the form-->
+              <!-- has successfully submitted-->
+              <div class="d-none" id="submitSuccessMessage">
+                <div class="text-center mb-3">
+                  <div class="fw-bolder">Form submission successful!</div>
+                  To activate this form, sign up at
+                  <br />
+                  <a
+                    href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
                 </div>
-                <!-- Message input-->
-                <div class="form-floating mb-3">
-                  <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..."
-                    style="height: 10rem" data-sb-validations="required"></textarea>
-                  <label for="message">Message</label>
-                  <div class="invalid-feedback" data-sb-feedback="message:required">A message is
-                    required.</div>
-                </div>
-                <!-- Submit success message-->
-                <!---->
-                <!-- This is what your users will see when the form-->
-                <!-- has successfully submitted-->
-                <div class="d-none" id="submitSuccessMessage">
-                  <div class="text-center mb-3">
-                    <div class="fw-bolder">Form submission successful!</div>
-                    To activate this form, sign up at
-                    <br />
-                    <a
-                      href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                  </div>
-                </div>
-                <!-- Submit error message-->
-                <!---->
-                <!-- This is what your users will see when there is-->
-                <!-- an error submitting the form-->
-                <div class="d-none" id="submitErrorMessage">
-                  <div class="text-center text-danger mb-3">Error sending message!</div>
-                </div>
-                <!-- Submit Button-->
-                <div class="d-grid"><button class="btn btn-primary btn-xl disabled" id="submitButton"
-                    type="submit">Submit</button></div>
-              </form>
-            </div>
+              </div>
+              <!-- Submit error message-->
+              <!---->
+              <!-- This is what your users will see when there is-->
+              <!-- an error submitting the form-->
+              <div class="d-none" id="submitErrorMessage">
+                <div class="text-center text-danger mb-3">Error sending message!</div>
+              </div>
+              <!-- Submit Button-->
+              <div class="d-grid"><button class="btn btn-primary btn-xl disabled" id="submitButton"
+                  type="submit">Submit</button></div>
+            </form>
           </div>
         </div>
       </div>
@@ -261,8 +274,15 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
+import { GoogleMap, Marker, InfoWindow } from "vue3-google-map";
+
+const center = { lat: 22.62808625882824, lng: 120.29252321578828 };
+const infoWindow = { lat: 22.62808625882000, lng: 120.29252321578828 };
 
 const mybutton = ref(null);
+
+
+
 
 //返回頁頂
 function backToTop() {
