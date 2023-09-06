50729317262a7c242d4ad40cf71f5f276c85769f
ChrislaFolia <frank08ta@gmail.com>
2023-09-07 01:00:07 +0800

    finished pagination funcrion in courseView

--- src/components/course/courseCard.vue
+++ src/components/course/courseCard.vue
@@ -1,65 +1,77 @@
 <template>
-    <div class="card h-100">
-        <img :src="`https://picsum.photos/300/200?random=${cardAmount}`" class="card-img-top mt-3" alt="..." />
-        <div class="card-body">
-            <span v-if="isLike == true" type="button" class="bi bi-heart-fill mx-1 "></span>
-            <span v-else type="button" class="bi bi-heart mx-1"></span>
-            <h5 class="card-text">課程名稱: {{ course.courseName }}</h5>
-            <div class="card-text mb-1">課程分類:{{ course.courseCategories.categoryName }}</div>
-            <div class="card-text mb-1">NT$ 100 起</div>
-            <div class="d-flex justify-content-end">
-                <button class="btn btn btn-primary"><i type="button" class="bi bi-lightning-fill"></i>&nbsp;前往選課</button>
-            </div>
-        </div>
+  <div class="card h-100">
+    <img
+      :src="`https://picsum.photos/300/200?random=${cardAmount}`"
+      class="card-img-top mt-3"
+      alt="..."
+    />
+    <div class="card-body">
+      <span
+        v-if="isLike == true"
+        type="button"
+        class="bi bi-heart-fill mx-1"
+      ></span>
+      <span v-else type="button" class="bi bi-heart mx-1"></span>
+      <h5 class="card-text">課程名稱: {{ course.courseName }}</h5>
+      <div class="card-text mb-1">
+        課程分類:{{ course.courseCategories.categoryName }}
+      </div>
+      <div class="card-text mb-1">NT$ 100 起</div>
+      <div class="d-flex justify-content-end">
+        <button class="btn btn btn-primary">
+          <i type="button" class="bi bi-lightning-fill"></i>&nbsp;前往選課
+        </button>
+      </div>
     </div>
+  </div>
 </template>
-    
+
 <script setup>
 const props = defineProps({
-    isLike: Boolean,
-    cardAmount: Number,
-    course: Object,
-})
+  isLike: Boolean,
+  cardAmount: Number,
+  course: Object,
+});
 // console.log(props.cardAmount);
 </script>
-    
+
 <style scoped>
 .btn-primary {
-    --bs-btn-color: #fff;
-    --bs-btn-bg: #FFC408;
-    --bs-btn-border-color: #fff;
-    --bs-btn-hover-bg: #E83015;
-    --bs-btn-hover-border-color: #c34e2e;
-    --bs-btn-active-bg: #c34e2e;
+  --bs-btn-color: #fff;
+  --bs-btn-bg: #ffc408;
+  --bs-btn-border-color: #fff;
+  --bs-btn-hover-bg: #e83015;
+  --bs-btn-hover-border-color: #c34e2e;
+  --bs-btn-active-bg: #c34e2e;
 }
 
 .card {
-    --bs-card-spacer-y: 1rem;
-    --bs-card-spacer-x: 1rem;
-    --bs-card-title-spacer-y: 0.5rem;
-    --bs-card-border-width: 1px;
-    --bs-card-border-color: var(--bs-border-color-translucent);
-    --bs-card-border-radius: 0.375rem;
-    --bs-card-box-shadow: ;
-    --bs-card-inner-border-radius: calc(0.375rem - 1px);
-    --bs-card-cap-padding-y: 0.5rem;
-    --bs-card-cap-padding-x: 1rem;
-    --bs-card-cap-bg: rgba(0, 0, 0, 0.03);
-    --bs-card-cap-color: ;
-    --bs-card-height: ;
-    --bs-card-color: ;
-    --bs-card-bg: #fff;
-    --bs-card-img-overlay-padding: 1rem;
-    --bs-card-group-margin: 0.75rem;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    min-width: 0;
-    height: var(--bs-card-height);
-    word-wrap: break-word;
-    background-color: var(--bs-card-bg);
-    background-clip: border-box;
-    border: var(--bs-card-border-width) solid var(--bs-card-border-color);
-    border-radius: var(--bs-card-border-radius);
+  --bs-card-spacer-y: 1rem;
+  --bs-card-spacer-x: 1rem;
+  --bs-card-title-spacer-y: 0.5rem;
+  --bs-card-border-width: 1px;
+  --bs-card-border-color: var(--bs-border-color-translucent);
+  --bs-card-border-radius: 0.375rem;
+  --bs-card-box-shadow: ;
+  --bs-card-inner-border-radius: calc(0.375rem - 1px);
+  --bs-card-cap-padding-y: 0.5rem;
+  --bs-card-cap-padding-x: 1rem;
+  --bs-card-cap-bg: rgba(0, 0, 0, 0.03);
+  --bs-card-cap-color: ;
+  --bs-card-height: ;
+  --bs-card-color: ;
+  --bs-card-bg: #fff;
+  --bs-card-img-overlay-padding: 1rem;
+  --bs-card-group-margin: 0.75rem;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-width: 0;
+  height: var(--bs-card-height);
+  word-wrap: break-word;
+  background-color: var(--bs-card-bg);
+  background-clip: border-box;
+  border: var(--bs-card-border-width) solid var(--bs-card-border-color);
+  border-radius: var(--bs-card-border-radius);
 }
 </style>
--- src/views/course.vue
+++ src/views/course.vue
@@ -1,82 +1,133 @@
 <template>
-    <div class="masthead">
-        <div id="carouselIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="https://picsum.photos/300/200?random=10" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="https://picsum.photos/300/200?random=11" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="https://picsum.photos/300/200?random=12" class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+  <div class="masthead">
+    <div id="carouselIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselIndicators"
+          data-bs-slide-to="0"
+          class="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselIndicators"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            src="https://picsum.photos/1400/900?random=10"
+            class="d-block w-100"
+            alt="..."
+          />
         </div>
+        <div class="carousel-item">
+          <img
+            src="https://picsum.photos/1400/900?random=11"
+            class="d-block w-100"
+            alt="..."
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="https://picsum.photos/1400/900?random=12"
+            class="d-block w-100"
+            alt="..."
+          />
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselIndicators"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselIndicators"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
+  </div>
 
-
-    <section class="page-section" id="courseView">
-        <div class="container">
-            <!-- SideBar -->
-            <div class="row">
-                <div class="col-2 ">
-                    <listGroup class="d-flex justify-content-center" :allCourseCategories="allCourseCategories" tabindex="0"
-                        v-focus></listGroup>
-                </div>
-
-                <!-- cards for course -->
-                <div class="col-10">
-                    <h1 class="text-center mb-4">全部課程列表</h1>
-                    <!-- <input type="text" v-focus> -->
-                    <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 justify-content-center">
-                        <courseCard v-for="( course, index ) in pageCourses" class="col-3 mx-2 my-3" :cardAmount="index"
-                            :course="course" :isLike="isLike"></courseCard>
-                    </div>
-
-                    <pagination v-if="paginationData.showPagination" :totalPages="paginationData.totalPages"
-                        class="d-flex justify-content-center mt-3"
-                        @clickNaborCoursePage-emit(nextOrLast,pageChoose)="changePage(nextOrLast, pageChoose)"
-                        @clickAnyCoursePage-emit(nextOrLast,pageChoose)="changePage(nextOrLast, pageChoose)">
-                    </pagination>
-                </div>
-            </div>
-
+  <section class="page-section" id="courseView">
+    <div class="container">
+      <!-- SideBar -->
+      <div class="row">
+        <div class="col-2">
+          <listGroup
+            class="d-flex justify-content-center"
+            :allCourseCategories="allCourseCategories"
+            tabindex="0"
+            v-focus
+          ></listGroup>
         </div>
-    </section>
 
-    <router-link class="btn btn-secondary mt-6" to="/course/detail">課程詳細資訊</router-link>
+        <!-- cards for course -->
+        <div class="col-10">
+          <h1 class="text-center mb-4">全部課程列表</h1>
+          <!-- <input type="text" v-focus> -->
+          <div
+            class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 justify-content-center"
+          >
+            <courseCard
+              v-for="(course, index) in pageCourses"
+              class="col-3 mx-2 my-3"
+              :cardAmount="index"
+              :course="course"
+              :isLike="isLike"
+            ></courseCard>
+          </div>
+
+          <pagination
+            v-if="paginationData.showPagination"
+            :totalPages="paginationData.totalPages"
+            :page="paginationData.page"
+            class="d-flex justify-content-center mt-3"
+            @clickNaborCoursePage-emit="changePage"
+            @clickAnyCoursePage-emit="changePage"
+          >
+          </pagination>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <router-link class="btn btn-secondary mt-6" to="/course/detail"
+    >課程詳細資訊</router-link
+  >
 </template>
 
 <script setup>
 /*
   imports
 */
-import { ref, reactive, onMounted } from 'vue';
-import axios from 'axios';
-import courseCard from '../components/course/courseCard.vue'
-import pagination from '../components/util/pagination.vue'
-import listGroup from '../components/util/listGroup.vue'
-import { vFocus } from '../directives/vFocus'
+import { ref, reactive, onMounted } from "vue";
+import axios from "axios";
+import courseCard from "../components/course/courseCard.vue";
+import pagination from "../components/util/pagination.vue";
+import listGroup from "../components/util/listGroup.vue";
+import { vFocus } from "../directives/vFocus";
 const URL = import.meta.env.VITE_API_JAVAURL;
 
-let isLike = ref(false)
+let isLike = ref(false);
 
 /*
   Load Datas
@@ -85,62 +136,69 @@ let isLike = ref(false)
 // Load courseCategories data
 const allCourseCategories = ref([]);
 const loadAllCourseCategories = async () => {
-    const URLAPI = `${URL}/coursecategories/findAll`;
-    const response = await axios.get(URLAPI);
-    // console.log(response.data)
+  const URLAPI = `${URL}/coursecategories/findAll`;
+  const response = await axios.get(URLAPI);
+  // console.log(response.data)
 
-    //取得所有分類放進allCourseCategories變數
-    allCourseCategories.value = response.data;
-    // console.log(allCourseCategories)
+  //取得所有分類放進allCourseCategories變數
+  allCourseCategories.value = response.data;
+  // console.log(allCourseCategories)
 };
 
 // Load course data
 const pageCourses = ref([]);
 const paginationData = reactive({
-    page: 1,
-    totalPages: 1,
-    numberOfCourses: 6,
-    showPagination: false,
-})
+  page: 1,
+  totalPages: 1,
+  numberOfCourses: 6,
+  showPagination: false,
+});
 // const totalPages = ref(1);
 // const numberOfCourses = ref(6)
 
 const loadPageCourses = async () => {
-    const URLAPI = `${URL}/course/page`;
-    const response = await axios.get(URLAPI, {
-        params: {
-            p: paginationData.page,
-        }
-    });
+  const URLAPI = `${URL}/course/page`;
+  const response = await axios.get(URLAPI, {
+    params: {
+      p: paginationData.page,
+    },
+  });
 
-    //取得所有課程放進courses變數
-    pageCourses.value = response.data;
+  //取得所有課程放進courses變數
+  pageCourses.value = response.data;
 
-    //取得所有課程頁數及單頁資料數放進courses變數
-    paginationData.totalPages = parseInt(response.headers['total-pages']);
-    paginationData.numberOfCourses = parseInt(response.headers['number-of-elements']);
-    paginationData.showPagination = true // loadPageCourses後，v-if顯示pagination
+  //取得所有課程頁數及單頁資料數放進courses變數
+  paginationData.totalPages = parseInt(response.headers["total-pages"]);
+  paginationData.numberOfCourses = parseInt(
+    response.headers["number-of-elements"]
+  );
+  paginationData.showPagination = true; // loadPageCourses後，v-if顯示pagination
 };
 
 //trigger @click pagination後換頁
 const changePage = (nextOrLast, pageChoose) => {
-
-    if (nextOrLast == 0) {
-        paginationData.page = pageChoose
-        loadPageCourses()
-    } else if (pageChoose == 0 && page != 1 && page != paginationData.totalPages) {
-        paginationData.page += nextOrLast
-        loadPageCourses()
-    } else {
-        alert('OOO')
-    }
-}
+  console.log(
+    "In Main Page, arg1 is: " + nextOrLast + " ,arg2 is: " + pageChoose
+  );
+  if (nextOrLast == 0 && paginationData.page != pageChoose) {
+    paginationData.page = pageChoose;
+    loadPageCourses();
+  } else if (
+    pageChoose == 0 &&
+    paginationData.page * nextOrLast != -1 &&
+    paginationData.page + nextOrLast <= paginationData.totalPages
+  ) {
+    paginationData.page += nextOrLast;
+    loadPageCourses();
+  } else {
+    console.log("do nothing");
+  }
+};
 
 onMounted(() => {
-    loadAllCourseCategories()
-    loadPageCourses()
-})
-
+  loadAllCourseCategories();
+  loadPageCourses();
+});
 </script>
 
 <style scoped></style>
