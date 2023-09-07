a5be7e0c8478864984e4c1f755ef27ebc94c3f4b
william <cc2260112@gmail.com>
2023-09-07 23:48:28 +0800

    修改登入 註冊畫面

--- src/views/register.vue
+++ src/views/register.vue
@@ -50,7 +50,7 @@ const register = async () => {
     return;
   }
 
-  if(!verifystatus){
+  if (!verifystatus) {
     alert("請驗證信箱")
     return;
   }
@@ -58,7 +58,7 @@ const register = async () => {
   try {
     const response = await axios.post(`${url}/members`, loginData);
   } catch (error) {
-    
+
   }
 };
 
@@ -116,44 +116,104 @@ const startCountdown = () => {
 };
 </script>
 
-
-
-
 <template>
-  <div class="masthead">
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-          aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-          aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-          aria-label="Slide 3"></button>
+  <section class="vh-100" style="background-color: #c1c1ae;">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-xl-10">
+          <div class="card" style="border-radius: 1rem;">
+            <div class="row g-0">
+              <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                <div class="card-body p-4 p-lg-5 text-black">
+                  <!-- 信箱 -->
+                  <div>
+                    <label class="form-label mb-1">信箱</label><span v-if="!loginData.memberemail"
+                      class="text-danger">*</span><input type="email" class="form-control form-control-lg"
+                      v-model="loginData.memberemail" placeholder="abc123456@example.com" />
+                    <div class="my-lg-1">
+                      <input type="button" v-model="verifybutton.button" @click="sendVerificationCode" id="verifybtn">
+                    </div>
+                    <input type="text" placeholder="輸入驗證碼" v-model="verify.verificationcode">
+                    <input type="button" value="驗證" @click="checkVerificationCode">
+                    <label v-if="verifystatus">驗證成功</label>
+                  </div>
+
+                  <!-- 密碼 -->
+                  <div class="mb-1">
+                    <label class="form-label">密碼</label>
+                    <span v-if="!loginData.memberpassword" class="text-danger">*</span>
+                    <input type="password" class="form-control" v-model="loginData.memberpassword" />
+                  </div>
+
+                  <!-- 手機號碼 -->
+                  <div class="mb-1">
+                    <label class="form-label">手機號碼</label>
+                    <span v-if="!loginData.memberphoneno" class="text-danger">*</span>
+                    <input type="email" class="form-control" v-model="loginData.memberphoneno" />
+                  </div>
+
+                  <!-- 姓名 -->
+                  <div class="mb-1">
+                    <label class="form-label">姓名</label>
+                    <span v-if="!loginData.membername" class="text-danger">*</span>
+                    <input type="email" class="form-control" v-model="loginData.membername" />
+                  </div>
+
+                  <!-- 性別 -->
+                  <div class="mb-1">
+                    <label class="form-label">性別</label>
+                    <span v-if="!loginData.membergender" class="text-danger">*</span>
+                    <input style="width: 1em;" type="radio" name="gender" value="男" v-model="loginData.membergender" />男
+                    <input style="width: 1em;" type="radio" name="gender" value="女" v-model="loginData.membergender" />女
+                  </div>
+
+                  <!-- 縣市 -->
+                  <div class="mb-1">
+                    <label class="form-label">縣市</label>
+                    <span v-if="!loginData.membercity" class="text-danger">*</span>
+                    <input type="text" class="form-control" v-model="loginData.membercity" />
+                  </div>
+
+                  <!-- 地區 -->
+                  <div class="mb-1">
+                    <label class="form-label">地區</label>
+                    <span v-if="!loginData.memberzone" class="text-danger">*</span>
+                    <input type="text" class="form-control" v-model="loginData.memberzone" />
+                  </div>
+
+                  <!-- 地址 -->
+                  <div class="mb-1">
+                    <label class="form-label">地址</label>
+                    <span v-if="!loginData.memberaddress" class="text-danger">*</span>
+                    <input type="text" class="form-control" v-model="loginData.memberaddress" />
+                  </div>
+
+                  <!-- 生日 -->
+                  <div class="mb-1">
+                    <label class="form-label">生日</label>
+                    <span v-if="!loginData.memberbirthday" class="text-danger">*</span>
+                    <input type="date" class="form-control" v-model="loginData.memberbirthday" />
+                  </div>
+
+                  <div class="pt-1 mb-1">
+                    <button class="btn btn-dark btn-lg btn-block" type="button" @click="submit()">註冊</button>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-5 d-none d-md-block">
+                <img src="../assets/index/other/4.jpg" alt="login form" class="img-fluid"
+                  style="border-radius: 0 1rem 1rem 0;" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="../assets/index/classroom/攀岩教室.jpg" class="d-block w-100" alt="..." />
-        </div>
-        <div class="carousel-item">
-          <img src="../assets/index/classroom/有氧大教室.jpg" class="d-block w-100" alt="..." />
-        </div>
-        <div class="carousel-item">
-          <img src="../assets/index/classroom/空中瑜珈.jpg" class="d-block w-100" alt="..." />
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-        data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-        data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
     </div>
-  </div>
-  <section class="page-section" id="about">
+  </section>
+
+
+
+  <!-- <section class="page-section" id="about">
     <div class="container">
       <div class="col-lg-4 col-md-4">
         <label class="">信箱</label>
@@ -164,11 +224,13 @@ const startCountdown = () => {
         <input type="button" value="驗證" @click="checkVerificationCode">
         <label v-if="verifystatus">驗證成功</label>
       </div>
+
       <div class="col-lg-4 col-md-4">
         <label class="">密碼</label>
         <span v-if="!loginData.memberpassword" class="text-danger">*</span>
         <input type="password" class="form-control" v-model="loginData.memberpassword" />
       </div>
+
       <div class="col-lg-4 col-md-4">
         <label class="">手機號碼</label>
         <span v-if="!loginData.memberphoneno" class="text-danger">*</span>
@@ -179,6 +241,7 @@ const startCountdown = () => {
         <span v-if="!loginData.membername" class="text-danger">*</span>
         <input type="email" class="form-control" v-model="loginData.membername" />
       </div>
+
       <div class="col-lg-4 col-md-4">
         <label class="">性別</label>
         <span v-if="!loginData.membergender" class="text-danger">*</span>
@@ -209,5 +272,5 @@ const startCountdown = () => {
         <input type="button" value="註冊" @click="register" />
       </div>
     </div>
-  </section>
+  </section> -->
 </template>
